16d403949f6cce610bd3e8c0a7781451c9d38679
eks2080 <eks2080@nyu.edu>
2024-05-01 20:41:23 -0400

    Updating concept and data

---
+++ js/bookstoredata.js
@@ -0,0 +1,82 @@
+var bookstoredata = [
+    {
+        "Name": "Books Are Magic",
+        "Location": "Cobble Hill",
+        "Borough": "Brooklyn",
+        "Longitude": -73.99155025,
+        "Latitude": 40.68428836,
+        "date": "2017"
+    },
+    {
+        "Name": "Astoria Bookshop",
+        "Location": "Astoria",
+        "Borough": "Queens",
+        "Longitude": -73.92958433089494,
+        "Latitude": 40.756975194229796,
+        "date": "2013"
+    }, 
+    {
+        "Name": "Yu & Me",
+        "Location": "Chinatown",
+        "Borough": "Manhattan",
+        "Longitude": -73.99902673,
+        "Latitude": 40.71515988,
+        "date": "2021"
+    },
+    {
+        "Name": "McNally Jackson",
+        "Location": "Seaport",
+        "Borough": "Manhattan",
+        "Longitude": -74.00357521,
+        "Latitude": 40.70650574,
+        "date": "2019"
+    },
+    {
+        "Name": "Sweet Pickle Books",
+        "Location": "Lower East Side",
+        "Borough": "Manhattan",
+        "Longitude": -73.99112591462918,
+        "Latitude": 40.71656750277776,
+        "date": "2020"
+    },
+    {
+        "Name": "Bluestockings Cooperative Bookstore",
+        "Location": "Lower East Side",
+        "Borough": "Manhattan",
+        "Longitude": -73.98591844523474,
+        "Latitude": 40.71958256793716,
+        "date": "1999"
+    },
+    {
+        "Name": "The Strand",
+        "Location": "Union Square",
+        "Borough": "Manhattan",
+        "Longitude": -73.99169605919626,
+        "Latitude": 40.73350490335699,
+        "date": "1927"
+    },
+    {
+        "Name": "Cafe Con Libros",
+        "Location": "Crown Heights",
+        "Borough": "Brooklyn",
+        "Longitude": -73.95184481275605,
+        "Latitude": 40.67470108825182,
+        "date": "2017"
+    },
+        {
+        "Name": "Sister's Uptown Bookstore",
+        "Location": "Washington Heights",
+        "Borough": "Manhattan",
+        "Longitude": -73.94147671549773,
+        "Latitude": 40.832976004077544,
+        "date": "2000"
+    }, 
+    {
+        "Name": "The Lit. Bar",
+        "Location": "Mott Haven",
+        "Borough": "Bronx",
+        "Longitude": -73.92705296121468,
+        "Latitude": 40.80769678982969,
+        "date": "2019"
+    } 
+]
---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+//Setting up Mapbox
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWtzMjA4MCIsImEiOiJjbHVsdWNmbTExNGg0MmtsZHVlOHN2Zzd5In0.wEYw-sKV39S4NkqO8CDQBw';
+
+var mapOptions = {
+    container: 'map-container', // container ID
+    style: 'mapbox://styles/mapbox/light-v11', // style URL
+    center: [-74.00011310930516, 40.72415607434748], // starting position [lng, lat]
+    zoom: 11, // starting zoom,
+}
+
+// instantiate the map
+const map = new mapboxgl.Map(mapOptions);
+
+// add a navitation control
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-right');
+
+// loop over the bookstoredata script to make a marker for each record
+bookstoredata.forEach(function (bookstoreRecord) {
+
+    var color
+
+    // use if statements to assign colors based on neighborhood
+
+    if (bookstoreRecord.Borough === 'Manhattan') {
+        color = '#9932CC'
+    }
+    if (bookstoreRecord.Borough === 'Queens') {
+        color = '#556B2F'
+    }
+    if (bookstoreRecord.Borough === 'Bronx') {
+        color = '#483D8B'
+    }
+    if (bookstoreRecord.Borough === 'Brooklyn') {
+        color = '#E9967A'
+    }
+
+    // create a popup to attach to the marker
+
+    const popup = new mapboxgl.Popup({
+        offset: 24,
+        anchor: 'bottom'
+    }).setHTML(
+        `<strong>${bookstoreRecord.Name}</strong> is located in <strong>${bookstoreRecord.Location}</strong>. The store opened in <strong>${bookstoreRecord.date}</strong>.`
+    );
+
+    // create a marker, set the coordinates, add the popup, add it to the map
+    new mapboxgl.Marker({
+        scale: 0.75,
+        color: color,
+    })
+        .setLngLat([bookstoreRecord.Longitude, bookstoreRecord.Latitude])
+        .setPopup(popup)
+        .addTo(map);
+})
